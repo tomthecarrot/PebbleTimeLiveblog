4410b4298b917b239b1b060d1154f826388c8d0f
Thomas Suarez <tomthecarrot@users.noreply.github.com>
2015-05-10 19:31:45 -0700

    Initial Commit

---
+++ src/main.c
@@ -0,0 +1,53 @@
+/*
+Liveblog App for Pebble Time.
+Code by Thomas Suarez (tomthecarrot)
+*/
+
+#include <pebble.h>
+
+static Window *s_main_window;
+static TextLayer *s_text_layer;
+  
+static void main_window_load(Window *window) {
+  // Create TextLayer
+  s_text_layer = text_layer_create(GRect(0, 45, 144, 100));
+  text_layer_set_background_color(s_text_layer, GColorClear);
+  text_layer_set_text_color(s_text_layer, GColorBlack);
+  text_layer_set_text(s_text_layer, "Subscribed:\nMaker Faire Bay Area 2015");
+  text_layer_set_overflow_mode(s_text_layer, GTextOverflowModeWordWrap);
+  text_layer_set_font(s_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
+  
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_text_layer));
+}
+
+static void main_window_unload(Window *window) {
+  // Destroy TextLayer
+  text_layer_destroy(s_text_layer);
+}
+
+static void init() {
+  // Create main Window element and assign to pointer
+  s_main_window = window_create();
+  
+  // Set handlers to manage the elements inside the widnow
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
+  });
+  
+  // Show the Window on the watch, animated=true
+  window_stack_push(s_main_window, true);
+}
+
+static void deinit() {
+  // Destroy window
+  window_destroy(s_main_window);
+}
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
+}
---
+++ src/timeline.js
@@ -0,0 +1,28 @@
+/*
+Liveblog App for Pebble Time.
+Code by Thomas Suarez (tomthecarrot)
+*/
+
+Pebble.addEventListener('ready',
+  function(e) {
+    console.log('JavaScript app ready and running!');
+    
+    /*Pebble.getTimelineToken(
+      function (token) {
+      console.log('My timeline token is ' + token);
+    },
+    function (error) { 
+      console.log('Error getting timeline token: ' + error);
+    }
+    );*/
+    
+    Pebble.timelineSubscribe('makerfaire-ba-2015', 
+      function () { 
+        console.log('Subscribed to topic!');
+      }, 
+      function (errorString) { 
+        console.log('Error subscribing to topic: ' + errorString);
+      }
+    );
+  }
+);
