43e3c227c8be5d41032395b313561f957a2c6726
Thomas Suarez <tomthecarrot@users.noreply.github.com>
2015-07-01 20:00:47 -0700

    Client overhaul, prepare for menu.

--- src/main.c
+++ src/main.c
@@ -5,45 +5,72 @@ Code by Thomas Suarez (tomthecarrot)
 
 #include <pebble.h>
 
-static Window *s_main_window;
-static TextLayer *s_text_layer;
+static char *subscription = ""; // name of the added subscription
+static Window *win_sub; // subscribe confirmation window
+static TextLayer *text_subscribed; // subscribe confirmation text
+
+static void win_sub_load(Window *window) {
+  // Create TextLayer & set parameters
+  text_subscribed = text_layer_create(GRect(0, 10, 144, 100));
+  text_layer_set_background_color(text_subscribed, GColorClear);
+  text_layer_set_text_color(text_subscribed, GColorBlack);
+  text_layer_set_overflow_mode(text_subscribed, GTextOverflowModeWordWrap);
+  text_layer_set_font(text_subscribed, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  text_layer_set_text_alignment(text_subscribed, GTextAlignmentCenter);
   
-static void main_window_load(Window *window) {
-  // Create TextLayer
-  s_text_layer = text_layer_create(GRect(0, 45, 144, 100));
-  text_layer_set_background_color(s_text_layer, GColorClear);
-  text_layer_set_text_color(s_text_layer, GColorBlack);
-  text_layer_set_text(s_text_layer, "Subscribed:\nMaker Faire Bay Area 2015");
-  text_layer_set_overflow_mode(s_text_layer, GTextOverflowModeWordWrap);
-  text_layer_set_font(s_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
-  text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
+  // Concatenate strings to create message & set text
+  static char message[400]; // buffer to hold concatenated strings
+  strcpy(message, "Subscribed to:\n"); // copy first part
+  strcat(message, subscription); // concatenate second part
+  text_layer_set_text(text_subscribed, message); // set text to view
   
-  // Add it as a child layer to the Window's root layer
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_text_layer));
+  // Add TextLayer as a child layer to the subscription Window's root layer
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_subscribed));
 }
 
-static void main_window_unload(Window *window) {
+static void win_sub_unload(Window *window) {
   // Destroy TextLayer
-  text_layer_destroy(s_text_layer);
+  text_layer_destroy(text_subscribed);
+}
+
+static void add_subscription(char *new) {
+  // Concatenate current and new subscription vars
+  char updated[400]; // buffer to hold concatenated strings
+  strcpy(updated, subscription);
+  strcat(updated, "\n- "); // concatenate newline and list item
+  strcat(updated, new); // concatenate new part
+  subscription = updated; // set globally
+  
+  // Register timeline listener for new subscription
+  //add here...
+}
+
+static void tmp() {
+  // tmp test
+  add_subscription("Verge Live");
+  add_subscription("Maker Faire BA 2015");
+  add_subscription("Twitter: @tomthecarrot");
 }
 
 static void init() {
+  tmp(); // tmp test
+  
   // Create main Window element and assign to pointer
-  s_main_window = window_create();
+  win_sub = window_create();
   
   // Set handlers to manage the elements inside the widnow
-  window_set_window_handlers(s_main_window, (WindowHandlers) {
-    .load = main_window_load,
-    .unload = main_window_unload
+  window_set_window_handlers(win_sub, (WindowHandlers) {
+    .load = win_sub_load,
+    .unload = win_sub_unload
   });
   
   // Show the Window on the watch, animated=true
-  window_stack_push(s_main_window, true);
+  window_stack_push(win_sub, true);
 }
 
 static void deinit() {
   // Destroy window
-  window_destroy(s_main_window);
+  window_destroy(win_sub);
 }
 
 int main(void) {
